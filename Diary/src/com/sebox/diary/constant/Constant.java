f5a81f0ed5909313aa0db691a76580bd23d31d94
Rayleigh <rayleighcl@gmail.com>
2014-06-26 23:36:46 +0800

    Constant

--- Diary/src/com/sebox/diary/constant/Constant.java
+++ Diary/src/com/sebox/diary/constant/Constant.java
@@ -2,5 +2,5 @@ package com.sebox.diary.constant;
 
 public class Constant {
 	public final static String TABLE_INFO = "create table DIARY_INFO"
-			+ "(date TEXT,week TEXT,weather TEXT,diarytitle TEXT,diaryinfo TEXT,id INTEGER PRIMARY KEY AUTOINCREMENT)";
+			+ "(date TEXT,week TEXT,weather TEXT, diarytitle TEXT, diaryinfo TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)";
 }
